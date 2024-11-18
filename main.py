68639aaee9153ae79a994b751351551a907eacb4
elikimz <elijahkimani1293@gmail.com>
2024-11-18 08:12:59 +0300

    commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import HTTPException,Depends
+from tkinter.tix import STATUS
+from fastapi import HTTPException,Depends, Response
 from random import randrange
 from fastapi import FastAPI
 from typing import Optional, Union
@@ -19,13 +20,11 @@ app=FastAPI()
 
 
 
-
-
 class Post(BaseModel):
     title: str
     content: str
-    publish: bool=False
-    rating:Optional[int]=None
+    published: bool=False
+    
     
 
 while True :
@@ -56,69 +55,15 @@ def my_index_delete(id):
  
 
 
-
-
-app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hello": "Welcome to my API server using python"}
-
-
-@app.get("/posts")
-def read_posts():
-    cursor.execute("""SELECT*FROM posts""")
-    posts=cursor.fetchall()
-    # print(posts)
-    return{"Data":posts}
-
 @app.post("/posts")
 def create_post(new_post:Post,db:Session=Depends(get_db)):
-    new_post=model.Post(title=new_post.title, content=new_post.content, published=new_post.publish)
+    # print(new_post.dict())
+    new_post=model.Post(**new_post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     return{"data":new_post}
-    # cursor.execute(
-    #     """INSERT INTO posts(title,content) values (%s,%s)RETURNING*""",
-    # (new_post.title,new_post.content)
-    # )
-    # latest_post=cursor.fetchone()
-    # conn.commit()
-    # # print(latest_post)
-    # return {"message":latest_post}
 
-
-    
-@app.get("/posts/{id}")
-def get_posts(id:int):
-    cursor.execute("""SELECT * FROM posts WHERE id=%s""",(str(id),))
-    latest=cursor.fetchone()
-    # print(latest)
-    if not latest:
-      raise HTTPException(status_code=404,detail= f"post with id {id} is not found")
-     
-    return {"message":latest}
-
-@app.delete("/post/{id}")
-def delete_post(id: int):  # Add `id` as a parameter to the function
-      cursor.execute("""DELETE FROM posts WHERE id=%sRETURNING*""",(str(id),))
-      deleted_post=cursor.fetchone()
-      conn.commit()
-      if not deleted_post:
-         raise HTTPException(status_code=404,detail= f"post with id {id} is not found")
-        
-      return {"message":deleted_post}
-
-@app.put("/posts/{id}") 
-def update_post(id:int,post:Post):
-    cursor.execute("""UPDATE posts SET title=%s,content=%s  WHERE id=%sRETURNING*""",(post.title,post.content,str(id),))
-    updated_post=cursor.fetchone()  
-    conn.commit()
-    if not updated_post:
-         raise HTTPException(status_code=404,detail= f"post with id {id} is not found")
-
-
-    return {"message":updated_post}
 
 @app.get("/sqlalchemy")
 def test_posts(db:Session=Depends(get_db)):
@@ -126,3 +71,41 @@ def test_posts(db:Session=Depends(get_db)):
     
 
       return {"message":posts}
+   
+
+    
+@app.get("/posts/{id}")
+def get_posts(id:int,db:Session=Depends(get_db)):
+    posts=db.query(model.Post).filter(model.Post.id==id).first()
+   
+    if not posts:
+      raise HTTPException(status_code=404,detail= f"post with id {id} is not found")
+     
+    return {"message":posts}
+
+
+@app.delete("/post/{id}")
+def delete_post(id: int,db:Session=Depends(get_db)):  # Add `id` as a parameter to the function
+     post=db.query(model.Post).filter(model.Post.id==id)
+     
+
+     if post.first()==None:
+         raise HTTPException(status_code=404,detail= f"post with id {id} is not found")
+     post.delete(synchronize_session=False)
+     db.commit()
+     return{"message":"deleted successifully"}
+        
+        
+
+
+@app.put("/posts/{id}") 
+def update_post(id:int,updated_post_data: Post,db:Session=Depends(get_db)):
+    updated_post=db.query(model.Post).filter(model.Post.id==id)
+    updated=updated_post.first()
+    if updated==None:
+         raise HTTPException(status_code=404,detail= f"post with id {id} is not found")
+    updated_post.update(updated_post_data.dict(),synchronize_session=False)
+    db.commit()
+
+
+    return {"message": "Post updated successfully", "data": updated_post.first()}
